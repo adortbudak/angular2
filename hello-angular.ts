81edb9a07082b6396b4b3ffee578b0bff0a77124
Alper Dortbudak <adortbudak@gmail.com>
2016-07-16 20:16:12 -0700

    creating first component

--- hello-angular.ts
+++ hello-angular.ts
@@ -1,6 +1,13 @@
 import {Component} from '@angular/core';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 
+@Component({
+    selector: 'hello-angular',
+    template: `
+        <h1> {{greeting}} <h1> 
+    `
+})
+
 class HelloAngularComponent {
     greeting: string;
     constructor(){
