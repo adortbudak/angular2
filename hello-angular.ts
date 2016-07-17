92174608717ceb946187c3b36cd0aa1f35ecd3c2
Alper Dortbudak <adortbudak@gmail.com>
2016-07-16 20:12:13 -0700

    setup continues

---
+++ hello-angular.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+class HelloAngularComponent {
+    greeting: string;
+    constructor(){
+        this.greeting = "Hello Angular 2!"
+    }
+}
