ed5e201eab77ed5751a4e39293771bb0f05d616b
Alper Dortbudak <adortbudak@gmail.com>
2016-07-17 11:11:32 -0500

    end of chapter 1

--- app/pomodoro-timer.ts
+++ app/pomodoro-timer.ts
@@ -5,7 +5,14 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 @Component({
     selector: 'pomodore-timer',
     template: `
-        <h1> {{minutes}}:{{seconds}} </h1>
+        <div class="text-center">
+        <h1> {{minutes}}:{{seconds | number: '2.0'}} </h1>
+        <p>
+            <button (click)="togglePause()" class="btn btn-danger">
+             {{buttonLabel}}
+            </button>
+        </p>
+        </div>
     `
 })
 
@@ -15,9 +22,40 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 class PomodoroTimerComponent {
     minutes: number;
     seconds: number;
+    buttonLabel: string;
+    isPaused: boolean;
     constructor() {
+        this.resetPomodoro();
+        setInterval(()=> this.tick(),1000);
+    }
+
+    tick(){
+        if (!this.isPaused){
+            this.buttonLabel = 'Pause';
+            if (--this.seconds < 0){
+            this.seconds = 59;
+            if (--this.minutes < 0)
+            {
+                this.resetPomodoro();
+            }
+        }
+        }
+        
+    }
+
+    resetPomodoro(){
         this.minutes = 24;
         this.seconds = 59;
+        this.buttonLabel = 'Start';
+        this.togglePause();
+    }
+
+    togglePause(){
+        this.isPaused = !this.isPaused;
+
+        if (this.minutes < 24 || this.seconds < 59){
+            this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+        }
     }
 }
 
