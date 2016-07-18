2c46e696002be24ebbd83b31d09338e03c5dace6
Alper Dortbudak <adortbudak@gmail.com>
2016-07-17 22:16:00 -0500

    task icon component

---
+++ app/boot.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import {TasksComponent} from './pomodoro-tasks';
+
+bootstrap(TasksComponent);
--- app/pomodoro-tasks.ts
+++ app/pomodoro-tasks.ts
@@ -1,5 +1,6 @@
 import {Component,Input,Pipe,PipeTransform,Directive,OnInit,HostListener} from '@angular/core';
-import {bootstrap} from '@angular/platform-browser-dynamic';
+import {I18nPluralPipe,NgLocalization} from '@angular/common';
+
 
 interface Task {
     name: string;
@@ -18,21 +19,25 @@ class TaskService {
             {
                 name: "Code an HTML Table",
                 deadline: "Jun 23 2015",
+                queued: false,
                 pomodorosRequired: 1
             },
             {
                 name: "Task 2",
                 deadline: "Jun 24 2015",
+                queued: false,
                 pomodorosRequired: 2
             },
             {
                 name: "Task 3",
                 deadline: "Jun 25 2015",
+                queued: false,
                 pomodorosRequired: 1
             },
             {
                 name: "Task 4",
                 deadline: "Jun 26 2015",
+                queued: true,
                 pomodorosRequired: 3
             }
         ];
@@ -41,7 +46,7 @@ class TaskService {
             return {
                 name: task.name,
                 deadline: new Date(task.deadline),
-                queued: false,
+                queued: task.queued,
                 pomodorosRequired: task.pomodorosRequired
             };
         });
@@ -49,24 +54,63 @@ class TaskService {
 }
 
 @Component({
+    selector:'pomodoro-task-icons',
+    template:`
+        <img *ngFor="let icon of icons" src="/assets/img/pomodoro.jpeg" width="50"/>
+    `
+})
+
+class TaskIconsComponent implements OnInit{
+    @Input() task: Task;
+    icons: Object[] = [];
+    ngOnInit(){
+        console.log(this.task.queued);
+        if (this.task.queued){
+            this.icons.length = this.task.pomodorosRequired;
+        }
+        else{
+            this.icons.length = 0;
+        }
+        this.icons.fill({name: this.task.deadline});
+    }
+}
+
+@Component({
     selector: 'pomodoro-tasks',
     styleUrls: ['./app/pomodoro-tasks.css'],
-    templateUrl: './app/pomodoro-tasks.html'
+    templateUrl: './app/pomodoro-tasks.html',
+    providers:[NgLocalization],
+    directives:[TaskIconsComponent]
 })
-class TasksComponent{
+export class TasksComponent{
     today: Date;
     tasks: Task[];
+    queuedPomodoros: number;
+    queueHeaderMapping: any = {
+        '=0': 'No pomodoros',
+        '=1': 'One pomodoro',
+        'other': '# pomodoros'
+    };
 
     constructor(){
         const taskService: TaskService = new TaskService();
         this.tasks = taskService.taskStore;
         this.today = new Date();
+        this.updateQueuedPomodoros();
 
     }
 
     toggleTask(task: Task){
         task.queued = !task.queued;
+        this.updateQueuedPomodoros();
+    }
+
+    private updateQueuedPomodoros(){
+        this.queuedPomodoros = this.tasks
+            .filter((task:Task) => task.queued)
+            .reduce((pomodoros: number,queuedTask:Task) => {
+                return pomodoros + queuedTask.pomodorosRequired;
+            },0);
     }
 }
 
-bootstrap(TasksComponent);
